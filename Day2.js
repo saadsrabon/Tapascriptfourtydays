e697c5592a9419924a15f1aaec54f61d548d2d15
Saad Srabon <saadsrabon2@gmail.com>
2025-08-29 04:47:53 +0600

    Day one and day2 done

--- Day2.js
+++ Day2.js
@@ -1,1 +1,27 @@
-Hello World
+// ✅ Task 1: Declare variables for a person’s name, age, isStudent status, and favorite programming language.
+const username = "Saad";
+const age = 24;
+const isStudent = false;
+const status = "Married";
+const favouriteProgrammingLanguage = "JavaScript";
+// ✅ Task 2: Print the values to the console.
+console.log(
+    username,
+    age,
+    isStudent,
+    status,
+    favouriteProgrammingLanguage
+);
+// ✅ Task 3: Try reassigning values to let and const variables and observe errors.
+// const username = "saad"
+// ✅ Task 4: Create an object and an array, assign them to new variables, modify, and observe changes.
+const user = {
+    username: "Saad",
+    age: age,
+    isStudent: false,
+};
+
+const userArray =[4,5,6,7];
+const newFile = userArray;
+
+console.log(userArray ,newFile)
