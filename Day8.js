f459a30b0acfabf6899a78ddd2b0c6d8fa348abd
Saad Srabon <saadsrabon2@gmail.com>
2025-09-01 10:06:44 +0600

    Execution context understand

--- Day8.js
+++ Day8.js
@@ -18,3 +18,41 @@ function getResult(a, b) {
 }
 
 getResult(8, 5);
+
+// GC
+//  CP 
+//   message = undefined
+//   f() body for sum 
+//   f(mul) body for mul
+//   f(calc) body for calc
+//   f() body getResult()
+//  EP
+//   message =  "I can do it"
+//  FEC forGetResult --
+    
+//        Cp-
+//          a-undifined
+//          b- undifined
+          
+//          Ep-
+//          a=8 ,b=5;
+//           FECcal-
+//           Cp-
+//          a-undifined
+//          b- undifined
+          
+//          Ep-
+//          a=8 ,b=5;
+//           FEC sum- 
+//            cp
+//              result = indefined
+//                a =undefinded
+//                b = undefined
+//              ec
+//                 result = 13
+//                a =5
+//                b = 8
+              
+               
+              
+            
