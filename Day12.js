2377a081aaa93f36e7f44510ce429f230ba0b0e7
Saad Srabon <saadsrabon2@gmail.com>
2025-09-07 23:13:23 +0600

    student management done

--- Day12.js
+++ Day12.js
@@ -14,3 +14,39 @@ console.log(obj1.b);// nothig will happen because we are trying to modify a froz
 const obj2 = Object.seal({ a: 1 });
 obj2.a = 2;
 console.log(obj2.a);// it will change
+
+// 3. Given an object with deeply nested properties, extract name, company, and address.city using destructuring
+const person = {
+  name: "Tapas",
+  company: {
+    name: "tapaScript",
+    location: {
+      city: "Bangalore",
+      zip: "94107"
+    }
+  }
+};
+
+const {name,company:{location:{city}}} =person;
+
+console.log(name,city);
+
+// 4. Build a Student Management System
+// Store student details in an object (name, age, grades).
+// Implement a method to calculate the average grade.
+
+ const student = {
+    name:"Student",
+    age:15,
+    grades:[10,10,10,10,5],
+    avgGrade:function (){
+        let result=0;
+        this.grades.map(grade=> result+=grade);
+
+        return result/this.grades.length;
+
+    }
+ }
+
+const avgGrade = student.avgGrade()
+console.log(avgGrade)
