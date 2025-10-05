d9d50892cab57fe3b440b466667e0a5915ef7f56
Saad Srabon <saadsrabon2@gmail.com>
2025-10-05 13:28:39 +0600

    files created

--- Day18.js
+++ Day18.js
@@ -1,6 +1,15 @@
 const selectItem = document.getElementById('field');
+const myForm =document.getElementById('myform')
 // amar hsse ekta field Create korte hbe
 function handleCreateFields(){
     console.log(selectItem.value);
+    let itemName =selectItem.value;
+    let createItem =document.createElement('input');
+    createItem.setAttribute('type',itemName);
+    myForm.appendChild(createItem);
+
+
 }
-selectItem.addEventListener('onchange',)
+selectItem.addEventListener('change',handleCreateFields)
+
+console.log("hello")
