c5b95aca7f8590d1cfa5e2945ccead551984aad1
Saad Srabon <saadsrabon2@gmail.com>
2025-10-05 13:18:10 +0600

    files uploaded

--- Day17.js
+++ Day17.js
@@ -13,3 +13,24 @@ document.body.appendChild(section)
 // Use document.querySelector() or getElementById() to select the paragraph.
 // Convert the text into an array of words.
 // Use querySelector() to display the most frequent word along with the count inside another <div>.
+
+
+// jebra
+
+const ul = document.querySelectorAll('ul li');
+const arrayUl =[...ul];
+console.log(arrayUl)
+for(let i =0; i<arrayUl.length; i++){
+    if(i%2==0){
+        arrayUl[i].style.color="white"
+        arrayUl[i].style.backgroundColor = "black";
+
+    }else{
+        arrayUl[i].style.color="black"
+        arrayUl[i].style.backgroundColor="white"
+    }
+}
+document.querySelectorAll('ul li').forEach((li, i) => {
+  li.style.color = i % 2 === 0 ? "white" : "black";
+  li.style.backgroundColor = i % 2 === 0 ? "black" : "white";
+});
---
+++ Day18.js
@@ -0,0 +1,6 @@
+const selectItem = document.getElementById('field');
+// amar hsse ekta field Create korte hbe
+function handleCreateFields(){
+    console.log(selectItem.value);
+}
+selectItem.addEventListener('onchange',)
