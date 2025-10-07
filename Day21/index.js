908bbaf7aac0de44a551eab5686e523bc1b03d0f
Saad Srabon <saadsrabon2@gmail.com>
2025-10-07 16:01:15 +0600

    files updated

---
+++ Day21/index.js
@@ -0,0 +1,27 @@
+const quizData = [
+  {
+      question: "What does DOM stand for?",
+      options: [
+          "Document Order Model",
+          "Document Object Model",
+          "Data Object Method",
+          "Direct Object Management"
+      ],
+      correct: 1
+  },
+  {
+      question: "Which method selects by ID?",
+      options: [
+        "getElementById()",
+        "querySelectorAll()",
+        "getElement()",
+        "getElementsByClassName()"
+      ],
+      correct: 0
+    },
+    {
+      question: "Which event fires on input change?",
+      options: ["click", "submit", "change", "keydown"],
+      correct: 2
+    }
+];
