e533f6954630cf10adea6734d2ef40d68a6f9596
Saad Srabon <saadsrabon2@gmail.com>
2025-10-12 03:09:25 +0600

    callback task two implemented

--- Day22.js
+++ Day22.js
@@ -9,3 +9,16 @@ const thank=(name,callback) =>
 thank('saad',()=>{
     console.log("Thank you");
 })
+
+// . Implement a calculator function that accepts two numbers and a callback to perform operations like add, subtract
+
+function calculator(a, b, operationCallback) {
+  // Complete this function
+ return operationCallback(a,b);
+}
+
+function add(x, y) {
+  return x + y;
+}
+
+ console.log(calculator(5, 3, add))
