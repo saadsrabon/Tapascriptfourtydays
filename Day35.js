aa5880e9aa88d197a8db7e341b388f432a143456
Saad Srabon <saadsrabon2@gmail.com>
2025-10-26 07:57:03 +0600

    Problem two solved

--- Day35.js
+++ Day35.js
@@ -1,15 +1,24 @@
 function makeMultipliers() {
   const result = [];
-  for (var i = 1; i <= 3; i++) {
+  for (let i = 1; i <= 3; i++) {
     result.push(function (num) {
       return num * i;
     });
   }
   return result;
-}
+}  /// var is function scoped we need to make it block scope
 
 const [double, triple, quadruple] = makeMultipliers();
 
-console.log(double(2));  // Expected: 2 * 1 = 2
-console.log(triple(2));  // Expected: 2 * 2 = 4
-console.log(quadruple(2)); // Expected: 2 * 3 = 6
+// console.log(double(2));  // Expected: 2 * 1 = 2
+// console.log(triple(2));  // Expected: 2 * 2 = 4
+// console.log(quadruple(2)); // Expected: 2 * 3 = 6
+
+// 
+for (var i = 0; i <= 3; i++) {
+   let data =i; // take a temporary variable as it will reasign everytime
+  setTimeout(function () {
+    
+    console.log("Count:", data);
+  }, 1000);
+}
