189d982520299806c9f74b354b6a3e7fad48a35f
Saad Srabon <saadsrabon2@gmail.com>
2025-10-29 10:44:05 +0600

    files updated

--- Day36.js
+++ Day36.js
@@ -20,9 +20,8 @@
 //     debounces(() => console.log('saad'), 5000)
 // );
 
-
 // // Practice the Throttling
-// /* 
+// /*
 //  So the key diffrence in throttling is the functions will call atleast once on that period of time
 //  we basically limit how often a function will be executed in certain time of period
 
@@ -39,24 +38,22 @@
 //    }}
 // }
 
-
-
 ///// Tasks////
 
 /// First Cahce the Items
-const input = document.getElementById('input')
-const counttext = document.getElementById('count')
+const input = document.getElementById("input");
+const counttext = document.getElementById("count");
 //add event listener to the input
 function countandShow(e) {
-  let string = new String(e.target.value)
+  let string = new String(e.target.value);
   if (string.length >= 200) {
-    alert('Warning: Keep it 200')
+    alert("Warning: Keep it 200");
   }
   showCount(string.length);
 }
-const throttledFunc = throttle(countandShow,5000)
-const debouncedFubc = funcdebounce(countandShow,500)
-input.addEventListener('keyup',debouncedFubc )
+const throttledFunc = throttle(countandShow, 5000);
+const debouncedFubc = funcdebounce(countandShow, 500);
+input.addEventListener("keyup", debouncedFubc);
 //show the count
 function showCount(length) {
   counttext.innerText = length;
@@ -68,27 +65,46 @@ function throttle(fn, interval) {
   return function (...args) {
     let now = Date.now();
     if (now - lastTtime >= interval) {
-      lastTtime =now;
-      return fn.apply(this, args)
+      lastTtime = now;
+      return fn.apply(this, args);
     }
-  }
+  };
 }
 
 //debounce function --
-function funcdebounce (fn,delay){
+function funcdebounce(fn, delay) {
   let timer;
-  return function(...args)
-  {
-    clearTimeout(timer)
-   timer = setTimeout(()=>{
-      fn.apply(this,args)
-    },delay)
-  }
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  };
 }
 
 //memoizing
-const memo =(fn)=>{
-  let cache ={}
-  if(resu)
-  const result =fn()
+///
+const square = (n) => {
+  return n * n;
+};
+
+function memo(fn) {
+  let cache = {};
+
+  return function (...args) {
+     const key = args.toString(); 
+    if (cache[key]) {
+      console.log('Return from Cache')
+      return cache[args];
+    } else {
+      let result = fn.bind(this, args);
+      cache[key]=result()
+      return result()
+    }
+  };
 }
+
+let memoed = memo(square)
+console.log(memoed(4))
+console.log(memoed(5))
+console.log(memoed(4))
